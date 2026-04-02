94cfdf996e06af835133269d21bb564aa17861e8
rpartida <rpartida@arizona.edu>
2026-04-02 15:31:05 -0700

    thres

--- main.py
+++ main.py
@@ -1890,7 +1890,7 @@ def _run_obstacle_detection_thread(
     FPS_LIMIT = 30
     should_quit = False
 
-    SAFE_DISTANCE_MM = 300
+    SAFE_DISTANCE_MM = 1800
     MIN_CONTOUR_AREA = 500
     MISSING_LINE_THRESHOLD = 30
 
